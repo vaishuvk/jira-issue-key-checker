c9dae6ce4d0a275fcdc5bd8689161b66606d0adc
vaishuvk <71807351+vaishuvk@users.noreply.github.com>
2023-07-11 14:57:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const jiraPrefix = core.getInput("jira-prefix");
+// const jiraPrefix = core.getInput("jira-prefix");
 
 async function run() {
   try {
     const prTitle = github.context.payload.pull_request.title;
     const prBody = github.context.payload.pull_request.body;
 
-    let regex = new RegExp(`${jiraPrefix}-[0-9]+`);
+    let regex = new RegExp(`[A-Z]-[0-9]+`);
     if (!regex.test(prTitle) || !regex.test(prBody)) {
       core.setFailed("Jira Issue Key missing in PR title or description.");
       return;
